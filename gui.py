601561a1e1ada290c9f7692a3dc9957b8d5f882b
JShadowMan <jshadowman@163.com>
2016-12-08 23:20:35 +0800

    refactor gui

--- gui.py
+++ gui.py
@@ -3,57 +3,33 @@
 # Copyright (C) 2016 ShadowMan
 #
 import asyncio
-import tkinter
 import threading
-from tkinter import ttk
+import tkinter as tk
+import tkinter.messagebox as messagebox
+import tkinter.ttk as ttk
 from bilibili import Package, Helper, Live
 
-standard_title = 'BiliBili Helper - ShadowMan'
-standard_window_size = (640, 480)
+_standard_title = 'BiliBili Helper - ShadowMan'
+_standard_window_size = (640, 480)
 
-class MessageHandler(Package.PackageHandlerProtocol):
-
-    def __init__(self):
-        super().__init__()
-
-    def on_allow_join(self):
-        print('join live room succeed')
-        return True
-
-    def on_dan_mu_message(self, contents):
-        print(contents)
-
-    def on_error_occurs(self, package):
-        pass
-
-    def on_gift_message(self, contents):
-        print(contents)
-
-    def on_heartbeat_response(self, contents):
-        print(contents)
-
-    def on_welcome_message(self, contents):
-        print(contents)
-
-class GuiApplication(tkinter.Frame):
+class GuiApplication(tk.Frame):
     
     def __init__(self, parent, async_loop = None):
-        tkinter.Frame.__init__(self, parent, background = 'white')
+        tk.Frame.__init__(self, parent, background = 'white')
 
         if async_loop is None:
             self.__async_loop = asyncio.get_event_loop()
         else:
             self.__async_loop = async_loop
 
-        self.parent = parent
-        self.style = tkinter.ttk.Style()
+        self.style = ttk.Style()
 
         self.__init_window()
 
     def __init_window(self):
-        self.parent.title(standard_title)
-        self.pack(fill = tkinter.BOTH, expand = 1)
-        self.__init_window_position(*standard_window_size)
+        self.master.title(_standard_title)
+        self.pack(fill = tk.BOTH, expand = True)
+        self.__init_window_position(*_standard_window_size)
         self.__init_window_style()
 
         self.__init_window_ui()
@@ -63,67 +39,92 @@ class GuiApplication(tkinter.Frame):
         self.style.configure('TButton', padding = (0, 5, 0, 5), font = 'Courier 12')
 
     def __init_window_ui(self):
-        self.pack(fill = tkinter.BOTH, expand = True)
+        self.__init_window_menu_bar()
+        self.__init_window_frame()
 
-        top_frame = tkinter.Frame(self)
-        top_frame.pack(fill = tkinter.X, side = tkinter.TOP)
+    def __init_window_menu_bar(self):
+        self.__menu_bar = tk.Menu(self.master)
+        self.master.config(menu = self.__menu_bar)
 
-        label_href_prefix = ttk.Label(top_frame, text = 'http://live.bilibili.com/')
-        label_href_prefix.pack(side = tkinter.LEFT, padx = 5, pady = 5)
+        file_menu = tk.Menu(self.__menu_bar, tearoff = False)
+        file_menu.add_command(label = 'Load...', underline = 0)
+        file_menu.add_command(label = 'Save...', underline = 0)
 
-        entry_value = tkinter.StringVar(top_frame)
-        entry_live_room_id = ttk.Entry(top_frame, textvariable = entry_value)
-        entry_live_room_id.pack(side = tkinter.LEFT, padx = 5, pady = 5)
+        view_menu = tk.Menu(self.__menu_bar, tearoff = False)
+        view_sub_menu_side_bar = tk.Menu(view_menu, tearoff = False)
+        view_sub_menu_side_bar.add_command(label = 'Hide Side Bar')
+        view_menu.add_cascade(label = 'Side Bar', menu = view_sub_menu_side_bar)
+        view_menu.add_command(label = 'Hide Status Bar')
 
-        btn_stop = ttk.Button(top_frame, text = 'Stop', command = self.stop)
-        btn_stop.pack(side = tkinter.RIGHT, padx = 5, pady = 5)
+        help_menu = tk.Menu(self.__menu_bar, tearoff = False)
+        help_menu.add_command(label = 'Documents')
+        help_menu.add_command(label = 'Weibo')
+        help_menu.add_separator()
+        help_menu.add_command(label = 'Open Source')
+        help_menu.add_command(label='License')
+        help_menu.add_separator()
+        help_menu.add_command(label='Check For Update')
+        help_menu.add_command(label='Changelog')
+        help_menu.add_command(label='About')
 
-        btn_start = ttk.Button(top_frame, text = 'Start', command = self.start)
-        btn_start.pack(side = tkinter.RIGHT, padx = 5, pady = 5)
+        self.__menu_bar.add_cascade(label = 'File', menu = file_menu, underline = 0)
+        self.__menu_bar.add_cascade(label = 'View', menu = view_menu, underline = 0)
+        self.__menu_bar.add_cascade(label = 'Help', menu = help_menu, underline = 0)
 
-        mid_frame = tkinter.Frame(self)
-        mid_frame.pack(fill = tkinter.BOTH, expand = True)
+    def __init_window_frame(self):
+        self.__init_window_top_frame()
+        self.__init_window_mid_frame()
 
-        text_value = tkinter.StringVar(mid_frame)
-        text_message = tkinter.Text(mid_frame)
-        text_message.pack(fill = tkinter.BOTH, expand = True)
+    def __init_window_top_frame(self):
+        top_frame = tk.Frame(self, background = '#fff', border = 1, relief = tk.SUNKEN)
 
-        bot_frame = tkinter.Frame(self)
-        bot_frame.pack(fill = tkinter.X, side = tkinter.BOTTOM)
+        label = tk.Label(top_frame, text = 'http://live.bilibili.com/', background = '#fff', font = 'Fira\ Code')
+        label.pack(side = tk.LEFT, padx = 3, pady = 3)
 
-        label_copyright = ttk.Label(bot_frame, text = 'Copyright (C) 2016 ShadowMan')
-        label_copyright.pack(side = tkinter.LEFT, padx = 5, pady = 10)
+        self.live_room_id = tk.StringVar(self.master, '00000')
+        entry = tk.Entry(top_frame, font = 'Fira\ Code', relief = tk.SUNKEN,
+                         width = 30, textvariable = self.live_room_id)
+        entry.pack(side = tk.LEFT, padx = 3, pady = 3)
 
-        label_open_source = ttk.Label(bot_frame, text = 'https://github.com/JShadowMan/bilibili')
-        label_open_source.pack(side = tkinter.RIGHT, padx = 5, pady = 10)
+        self.btn_text = tk.StringVar(self.master, 'Start')
+        btn_start = tk.Button(top_frame, text = 'Start', font = 'Fira\ Code',
+                         command = self.__startup, textvariable = self.btn_text)
+        btn_start.pack(side = tk.LEFT, padx = 3, pady = 3)
 
-        self.live_room_id = entry_value
-        self.message = text_value
+        top_frame.pack(side = tk.TOP, fill = tk.X)
 
-    def start(self):
-        live_room_id = self.live_room_id.get()
-        if not live_room_id:
+    def __init_window_mid_frame(self):
+        mid_frame = tk.Frame(self, background = '#fff', border = 1, relief = tk.FLAT)
+
+        text_dan_mu_message = tk.Text(mid_frame, font = 'Fira\ Code', cursor = 'trek')
+        text_dan_mu_message.pack(fill = tk.BOTH, expand = True)
+        self.dan_mu_message = text_dan_mu_message
+
+        mid_frame.pack(fill = tk.BOTH, expand = True)
+
+    def __startup(self):
+        if not self.live_room_id.get():
+            tk.messagebox.showwarning('Runtime Warning', 'live_room_id is none')
             return
 
-        live_room_id = int(live_room_id)
-        helper = Helper.bliHelper(loop = self.__async_loop)
-        threading.Thread(target = helper.async_startup,
-                                args = (Live.LiveBiliBili(loop = self.__async_loop).listen(live_room_id, MessageHandler),)).start()
+        if not self.live_room_id.get().isdigit():
+            tk.messagebox.showwarning('Runtime Warning', 'live_room_id must be integer')
+            return
+        tk.messagebox.showinfo('Listening', 'listening in {}'.format(self.live_room_id.get()))
 
-    def stop(self):
-        pass
+        self.dan_mu_message.insert(tk.END, '[SYSTEM] Listening in {}\n'.format(self.live_room_id.get()))
 
     def __init_window_position(self, width, height):
-        screen_width = self.parent.winfo_screenwidth()
-        screen_height = self.parent.winfo_screenheight()
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
 
         x_coordinate = int((screen_width - width) / 2)
         y_coordinate = int((screen_height - height) / 2)
 
-        self.parent.geometry('{}x{}+{}+{}'.format(width, height, x_coordinate, y_coordinate))
+        self.master.geometry('{}x{}+{}+{}'.format(width, height, x_coordinate, y_coordinate))
 
 if __name__ == '__main__':
-    root_window = tkinter.Tk()
+    root_window = tk.Tk()
     main_frame = GuiApplication(root_window)
 
     root_window.mainloop()
